87fd1774070b70b7b84737691c52bb750b1d8725
Leighton A Pitman <leightonpitman14@gmail.com>
2020-02-03 20:01:44 -0500

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,60 @@
 console.log('hello!');
+const http = require('http');
+const fs = require('fs');
+
+const readFile = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.toString());
+      }
+    });
+  });
+};
+
+const writeFile = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+const addGuest = guest => {
+  return readFile('./guests.json')
+    .then(data => {
+      const guests = JSON.parse(data);
+      let max = guests.reduce((acc, guest) => {
+        if (guest.id > acc) {
+          acc = guest.id;
+        }
+        return acc;
+      }, 0);
+      guest.id = max + 1;
+      guests.push(guest);
+      return writeFile('./guests.json', JSON.stringify(guests, null, 2));
+    })
+    .then(() => {
+      return guest;
+    });
+};
+
+http
+  .createServer((req, res) => {
+    if (req.url === '/api/users') {
+      readFile('./guests.json').then(data => {
+        console.log(data);
+        res.write(JSON.stringify(data));
+        res.end();
+      });
+    }
+    res.write('fail');
+    res.end();
+  })
+  .listen(3000);
